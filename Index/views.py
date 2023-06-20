21e2d0aad81519d3db9370959969f42cb3100fdf
Ishaan Nagpal <ishaannagpal@Ishaans-MacBook-Air.local>
2023-06-20 17:24:37 +0530

    first commit

---
+++ Index/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.urls import reverse
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import messages
+from django.conf import settings
+import uuid
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from .models import *
+from django.core.mail import send_mail
+from django.views import View
+
+
+def home(request):
+    return render(request, 'index.html')
+
