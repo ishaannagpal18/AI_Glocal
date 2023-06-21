68b6d32996892bcbfcab224455e5ac6c13834e07
Ishaan Nagpal <ishaannagpal@Ishaans-MacBook-Air.local>
2023-06-21 16:14:40 +0530

    first commit

--- Index/views.py
+++ Index/views.py
@@ -14,3 +14,11 @@ from django.views import View
 def home(request):
     return render(request, 'index.html')
 
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
